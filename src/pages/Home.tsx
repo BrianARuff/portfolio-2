41ddb833879aefde417270b1ef591fdc893bf7fc
BrianARuff <briananthonyruff@gmail.com>
2021-03-01 08:54:54 -0500

    updated h2 to h1 and changed content of it

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
-import "../styles/Home.scss"
-import { motion } from "framer-motion"
-import me from "../images/me-150x150.jpg"
-import { FunctionComponent } from "react"
+import "../styles/Home.scss";
+import { motion } from "framer-motion";
+import me from "../images/me-150x150.jpg";
+import { FunctionComponent } from "react";
 
 type Props = {
   animations: {
@@ -13,89 +13,93 @@ type Props = {
   }
 }
 
-const Home = ({animations}:Props) => {
+const Home = ({ animations }: Props) => {
   return (
-      <>
-          <article
-              className={`container first-body-article`}>
-            <motion.h2
-              initial="hidden"
-              animate="visible"
-              variants={animations.bodyHeader} 
-              className={`first-body-article-title`}>Welcome to Brian&apos;s portfolio!
-            </motion.h2>
-            <motion.img 
-                initial="hidden"
-                animate="visible"
-                variants={animations.imageOfMe} 
-                whileHover="hover"
-                whileTap="tap"
-                height="300px" 
-                width="300px" 
-                src={me} 
-                alt="pic of me" 
-                className={`pic-of-me`}
-            >
-            </motion.img>
-          </article>
+    <>
+      <article
+        className={`container first-body-article`}>
+        <motion.h2
+          initial="hidden"
+          animate="visible"
+          variants={animations.bodyHeader}
+          className={`first-body-article-title`}>Brian A. Ruff
+        </motion.h2>
+        <motion.img
+          initial="hidden"
+          animate="visible"
+          variants={animations.imageOfMe}
+          whileHover="hover"
+          whileTap="tap"
+          height="300px"
+          width="300px"
+          src={me}
+          alt="pic of me"
+          className={`pic-of-me`}
+        >
+        </motion.img>
+      </article>
 
-          <article className={`container`}>
-            <h3>Under Development</h3>
-            <motion.div 
-              animate={{
-                scale: 1,
-                rotate: [0, 360],
-                opacity: 1
-              }}
-              transition={{repeat: Infinity, duration: 3, ease: "linear"}} className={`react`}>
-            </motion.div>
-          </article>
-  
-          <motion.article 
+      <article className={`container`}>
+        <h3>Under Development</h3>
+        <motion.div
+          animate={{
+            scale: 1,
+            rotate: [0, 360],
+            opacity: 1
+          }}
+          transition={{ repeat: Infinity, duration: 3, ease: "linear" }} className={`react`}>
+        </motion.div>
+      </article>
+
+      <motion.article
+        initial="hidden"
+        animate="visible"
+        className={`second-home-article`}
+      >
+        <div className={`icons`}>
+          <motion.div
             initial="hidden"
-            animate="visible" 
-            className={`second-home-article`}
-          >
-            <div className={`icons`}>
-              <motion.div 
-                initial="hidden"
-                animate="visible"
-                variants={animations.imageLeft} 
-                className={`html`}>
-              </motion.div>
-              <motion.div animate={{
-                scale: [0, 1],
-                rotate: [0, 360],
-                opacity: [0, 1]}} className={`css`}></motion.div>
-              <motion.div animate={{
-                scale: [0, 1],
-                rotate: [0, 360],
-                opacity: [0, 1]}} className={`js`}></motion.div>
-              <motion.div animate={{
-                scale: 1,
-                rotate: [0, 360],
-                opacity: 1
-              }} className={`ts`}></motion.div>
-              <motion.div animate={{
-                scale: [0, 1],
-                rotate: [0, 360],
-                opacity: [0, 1]}} className={`node`}></motion.div>
-              <motion.div 
-                animate={{
-                  scale: [0, 1],
-                  rotate: [0, 360],
-                  opacity: [0, 1]}}
-                className={`angularjs`}></motion.div>
-              <motion.div 
-                initial="hidden"
-                animate="visible"
-                variants={animations.imageRight}
-                className={`framer`}></motion.div>
-            </div>
-          </motion.article>
-      </>
-  )
-}
+            animate="visible"
+            variants={animations.imageLeft}
+            className={`html`}>
+          </motion.div>
+          <motion.div animate={{
+            scale: [0, 1],
+            rotate: [0, 360],
+            opacity: [0, 1]
+          }} className={`css`} />
+          <motion.div animate={{
+            scale: [0, 1],
+            rotate: [0, 360],
+            opacity: [0, 1]
+          }} className={`js`} />
+          <motion.div animate={{
+            scale: 1,
+            rotate: [0, 360],
+            opacity: 1
+          }} className={`ts`} />
+          <motion.div animate={{
+            scale: [0, 1],
+            rotate: [0, 360],
+            opacity: [0, 1]
+          }} className={`node`} />
+          <motion.div
+            animate={{
+              scale: [0, 1],
+              rotate: [0, 360],
+              opacity: [0, 1]
+            }}
+            className={`angularjs`} />
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={animations.imageRight}
+            className={`framer`} />
+        </div>
+      </motion.article>
+    </>
+  );
+};
 
 
-export default Home
+export default Home;
